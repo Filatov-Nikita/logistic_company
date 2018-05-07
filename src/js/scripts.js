7228687da34fd7c128c816e48e5d4c0e436c7013
nikita <nikita45454@gmail.com>
2018-05-07 21:00:49 +0500

    err fixed

---
+++ src/js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+  $('.slider').slick({
+      arrows: false,
+      dots: true,
+      dotsClass: 'slick-dots1'
+  });
+  $('.slider1').slick({
+    arrows: false,
+    dots: true,
+    dotsClass: 'slick-dots1'
+});
+
+
+
+
+
+});
+          
