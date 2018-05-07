d8c396df66722875b83bbc9fec84096dfd3ec882
nikita <nikita45454@gmail.com>
2018-05-07 22:52:20 +0500

    ending

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,19 +1,44 @@
 $(document).ready(function() {
 
-  $('.slider').slick({
+  $('.slider1').slick({
       arrows: false,
       dots: true,
       dotsClass: 'slick-dots1'
   });
-  $('.slider1').slick({
-    arrows: false,
-    dots: true,
-    dotsClass: 'slick-dots1'
-});
 
-
-
-
-
+  $('.slider2').slick({
+    infinite: false,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+            arrows: false,
+            dots: true,
+            dotsClass: 'slick-dots1',
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+    });
 });
           
